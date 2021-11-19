7a5d2844ac04efcc5773bbf683cf527afbabb950
RodrigoARDSj <rodrigoalerez@gmail.com>
2021-11-19 20:15:21 -0300

    Projeto não finalizado

--- src/main/java/br/com/fiap/wastefood/DoNotWasteFoodApplication.java
+++ src/main/java/br/com/fiap/wastefood/DoNotWasteFoodApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableCaching
 public class DoNotWasteFoodApplication {
